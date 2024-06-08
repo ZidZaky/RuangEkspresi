007815b36a651a8307212ee7e783b6523af7eaac
Deo Farady S <deofarady43@gmail.com>
2024-06-08 08:50:47 +0700

    Update Sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -11,17 +11,15 @@
             margin: 0;
             padding: 0;
 
-
-
             .preview {
                 width: auto;
                 height: fit-content;
 
-                .preview-body {
-                    background-color: var(--bg);
-                    height: fit-content;
+                h4 {
+                    span {
+                        color: var(--accent);
+                    }
                 }
-
             }
 
             .menu {
@@ -41,19 +39,17 @@
 <body>
     @include('read.calendar')
 
-    <div class="preview mb-3 rounded-3 p-3 bg-white border border-secondary border-opacity-25">
-        <div class="preview-body p-3 rounded items-center d-flex flex-column align-items-center text-center rounded-3">
-            <img src="/assets/images/profile.png" alt="" class="mb-2" style="height:48px">
-            <h6 class="m-0 fw-medium" style="font-family: 'Inter';">Kevin Aluminium</h6>
-            <small class="text-secondary p-0 m-0 fst-italic fw-light" style="font-size: 14px;">@Kevin_Aluminium</small>
-        </div>
+    <div class="preview mb-3 rounded-3 p-4 border border-secondary border-opacity-25 d-flex flex-column gap-3 bg-white">
+        <h4 class="m-0 fw-semibold">Build <span>Meaningful</span> Relationships</h4>
+        <p class="m-0 text-secondary ">Connect and grow with like minded people.</p>
     </div>
+
     <div class="menu d-flex flex-column rounded border border-secondary border-opacity-25 gap-1 bg-white p-3">
         <a href="/dashboard" class="text-secondary rounded-2 text-decoration-none d-flex align-items-center gap-3 px-3 py-2"><i class="fi fi-rr-home d-flex"></i>Home</a>
         <hr class="m-0 border border-secondary border-opacity-25">
-        <a type="button" class="text-secondary rounded-3 text-decoration-none d-flex align-items-center gap-3 px-3 py-2" data-bs-toggle="modal" data-bs-target="#calendarModal"><i class="fi fi-rr-calendar-day d-flex"></i>Calender</a>
+        <a type="button" class="text-secondary rounded-2 text-decoration-none d-flex align-items-center gap-3 px-3 py-2" data-bs-toggle="modal" data-bs-target="#calendarModal"><i class="fi fi-rr-calendar-day d-flex"></i>Calender</a>
         <hr class="m-0 border border-secondary border-opacity-25">
-        <a href="/komunitas" class="text-secondary rounded-3 text-decoration-none d-flex align-items-center gap-3 px-3 py-2"><i class="fi fi-rr-users"></i>Community</a>
+        <a href="/komunitas" class="text-secondary rounded-2 text-decoration-none d-flex align-items-center gap-3 px-3 py-2"><i class="fi fi-rr-users"></i>Community</a>
     </div>
 </body>
 
